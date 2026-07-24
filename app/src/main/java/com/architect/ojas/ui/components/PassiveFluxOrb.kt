f85c553c2c4eb515cad8445d1f9d6a39aec5be5f
sandeep26dc <sandeep26dc@gmail.com>
2026-07-24 09:53:01 +0400

    Update PassiveFluxOrb.kt

--- app/src/main/java/com/architect/ojas/ui/components/PassiveFluxOrb.kt
+++ app/src/main/java/com/architect/ojas/ui/components/PassiveFluxOrb.kt
@@ -1,25 +1,21 @@
 package com.architect.ojas.ui.components
 
 import android.graphics.RuntimeShader
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ShaderBrush
 import com.architect.ojas.domain.model.OjasState
-import com.architect.ojas.ui.shader.FerrofluidShader
 
-@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
-fun PassiveFluxOrb(state: OjasState, time: Float) {
-    val shader = RuntimeShader(FerrofluidShader.CODE)
+fun PassiveFluxOrb(
+    state: OjasState,
+    shader: RuntimeShader
+) {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        shader.setFloatUniform("uSize", size.width, size.height)
-        shader.setFloatUniform("uTime", time)
-        shader.setFloatUniform("uMagneticFlux", state.magneticFieldIntensity)
-        shader.setFloatUniform("uGravity", state.gForceVector.first, state.gForceVector.second, state.gForceVector.third)
+        shader.setFloatUniform("uMagneticFlux", state.fluxIntensity)
+        shader.setFloatUniform("uGForce", state.tiltX, state.tiltY, state.tiltZ)
         drawRect(brush = ShaderBrush(shader))
     }
 }
